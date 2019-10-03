060e51ec20dbf6a4a54da11415b807072e6ac473
dujulien <du.julien1@gmail.com>
2019-10-03 17:31:11 +0200

    Exo Say Hello

--- 00_hello.rb
+++ 00_hello.rb
@@ -1,19 +1,20 @@
-def say_hello
-	first_name = gets.chomp
+def say_hello(first_name)
 	puts "Bonjour #{first_name}"
 end
 
+
 def ask_first_name
 	puts "Quel est ton prénom ?"
-	return say_hello
+	first_name = gets.chomp
+	return first_name
 end
 
 
 def perform
-  first_name = ask_first_name
-  hello = say_hello
-  say_hello(first_name)
-
+#  first_name = ask_first_name
+#  hello = say_hello
+  say_hello(ask_first_name)
 end
+
 
 perform
